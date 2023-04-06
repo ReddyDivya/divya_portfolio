7fd52ebfe908382d2c31aa8ef00c2b125b3e5e65
Divya <divyareddy0205@gmail.com>
2023-04-06 16:01:10 -0700

    about

---
+++ frontend_react/src/client.js
@@ -0,0 +1,24 @@
+//working with images using sanity
+import {createClient} from '@sanity/client';
+import ImageUrlBuilder  from '@sanity/image-url';
+
+// export const client = createClient({
+//   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+//   dataset: 'production',
+//   apiVersion: '2023-04-06',
+//   useCdn: true,
+//   token: process.env.REACT_APP_SANITY_TOKEN,
+// });
+
+
+export const client = createClient({
+  projectId: '8rkkr4xz',
+  dataset: 'production',
+  apiVersion: '2023-04-06',
+  useCdn: true,
+  token: 'skAbD8Pik1s2V4o98TvC9um8SOJuQY65QCMmaQryJpUd8Y9eTLbWoUYOtWBwQPLtPlTcBZudtb2gRi2Fz04NCF6pXQULdbWGJmYiiD0QCO718DcNM3oos0a84CpEpyXYUPtUYbsLUhYbUbfkOaj1mRxvshLt9BK38JyLMmtzBlBMRoNML2pt',
+});
+
+const builder = ImageUrlBuilder (client);
+
+export const urlFor = (source) => builder.image(source);
--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,35 +2,48 @@ import React, {useState, useEffect} from 'react';
 import './About.scss';
 import {motion} from "framer-motion";
 import {images} from "../../constants";
+import { urlFor, client } from '../../client';
 
-const abouts = [
-      {title:'Web Development', description: 'I am a good Web Developer', imgURL : images.about01 },
-      {title:'Frontend Development', description: 'I am a good Frontend Developer', imgURL : images.about02 },
-      {title:'UI/UX Design', description: 'I am a good UI/UX Design', imgURL : images.about04 }
-  ];
+// const abouts = [
+//       {title:'Web Development', description: 'Web development is one of my strong suits', imgURL : images.about01 },
+//       {title:'Frontend Development', description: 'My expertise lies in frontend development', imgURL : images.about02 },
+//       {title:'UI/UX Design', description: 'It is my pleasure to design UI/UX for my clients', imgURL : images.about04 }
+//   ];
 
 const About = () => {
+
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
+
   return (
     <>
-    <h2 className="head-text">I know that 
-    <span>Good Apps</span> <br/> means <span>Good Business</span></h2>
-    <div className="app__profiles">
-      {
-        abouts.map((about, index) => (
-          <motion.div whileInView={{ opacity: 1 }}
-          whileHover={{ scale: 1.1 }}
-          transition={{ duration: 0.5, type: 'tween' }}
-          className="app__profile-item"
-          key={about.title + index}>
-            <img src={about.imgURL} alt={about.title}/>
-            <h2 className="bold-text" style={{marginTop:20}}>{about.title}</h2>
-            <p className="p-text" style={{marginTop:10}}>{about.description}</p>
-          </motion.div> 
-        ))
-      }
-    </div>
+      <h2 className="head-text">I know that 
+      <span>Good Apps</span> <br/> means <span>Good Business</span></h2>
+      <div className="app__profiles">
+        {
+          abouts.map((about, index) => (
+            <motion.div whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
+            key={about.title + index}>
+              <img src={urlFor(about.imgUrl)} alt={about.title}/>
+              <h2 className="bold-text" style={{marginTop:20}}>{about.title}</h2>
+              <p className="p-text" style={{marginTop:10}}>{about.description}</p>
+            </motion.div> 
+          ))
+        }
+      </div>
     </>
   )
 }
 
-export default About
+export default About;
