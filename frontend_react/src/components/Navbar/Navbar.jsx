bd82f58fdd698018d6700b21440940e4ee4c4456
Divya <divyareddy0205@gmail.com>
2023-04-06 16:36:28 -0700

    about

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,55 +1,56 @@
-import React, {useState} from 'react';
-import {images} from "../../constants";
-import "./Navbar.scss";
-import {HiMenuAlt4, HiX} from "react-icons/hi";
-import {motion} from "framer-motion";
+import React, { useState } from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
+
+import { images } from '../../constants';
+import './Navbar.scss';
 
 const Navbar = () => {
-
   const [toggle, setToggle] = useState(false);
 
   return (
     <nav className="app__navbar">
-        <div className="app__navbar-logo">
-            <img src={images.logo} alt='logo'/>
-        </div>
-        <ul className="app__navbar-links">
-            {
-              /*menu*/
-            ['home', 'about', 'work', 'skills', 'contact'].map((item) =>
-              <li className="app__flex p-text" key={`link-${item}`}>
-                <div/>
-                  <a href={`#${item}`}>{item}</a>
-              </li>
-            )
-            }
-        </ul>
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
 
-        <div className="app__navbar-menu">
-          {/*displaying menu icon */}
-            <HiMenuAlt4 onClick={() => setToggle(true)}/> {/*setToggle = true - to open up menu */}
-            {
-            toggle && (
-              <motion.div whileInView={{x:[300, 0]}}
-              transition={{duration:0.85, ease:'easeOut'}}>
-                  {/*displaying X icon*/}
-                  <HiX onClick={() => setToggle(false)}/> {/*setToggle = false - to close menu */}
-                  <ul>
-                    {
-                      ['home', 'about', 'work', 'skills', 'contact'].map((item) => (
-                      <li key={item}>
-                          <a href={`#${item}`} onClick={() => setToggle(false)}>
-                            {item}
-                          </a>
-                      </li>))
-                    }
-                  </ul>
-              </motion.div>
-            )
-            }
-        </div>
+        {/*menu*/}
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="app__navbar-menu">
+
+         {/*displaying menu icon */}
+        <HiMenuAlt4 onClick={() => setToggle(true)} /> {/*setToggle = true - to open up menu */}
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+
+            {/*displaying X icon*/}
+            <HiX onClick={() => setToggle(false)} />  {/*setToggle = false - to close menu */}
+            <ul>
+              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
---
+++ frontend_react/src/components/NavigationDots.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {BsTwitter, BsInstagram} from "react-icons/bs";
+import {FaFacebookF} from "react-icons/fa";
+
+function NavigationDots({active}) {
+  return (
+    <div className="app__navigation">
+          
+    </div>
+  )
+}
+
+export default NavigationDots
