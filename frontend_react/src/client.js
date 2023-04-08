c9ab37e6201a8f733ce41f172877e988db4720f5
Divya <divyareddy0205@gmail.com>
2023-04-07 23:10:50 -0700

    works

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -4,15 +4,28 @@
 import {createClient} from '@sanity/client';
 import ImageUrlBuilder  from '@sanity/image-url';
 
-export const client = createClient({
-  projectId: '8rkkr4xz',
-  dataset: 'production',
-  apiVersion: '2023-04-07', // use current date (YYYY-MM-DD) to target the latest API version
-  useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,//Only if you want to update content with the client
+// export const client = createClient({
+//   projectId: '8rkkr4xz',
+//   dataset: 'production',
+//   apiVersion: '2023-04-07', // use current date (YYYY-MM-DD) to target the latest API version
+//   useCdn: true,
+//   token: process.env.REACT_APP_SANITY_TOKEN,//Only if you want to update content with the client
+// });
 
-});
+export async function getServerSideProps(context){
+  const client = createClient({
+    projectId: "8rkkr4xz",
+    dataset: "production",
+    useCdn: false,
+  });
 
-const builder = ImageUrlBuilder(client);
+  const query = `*[_type == "works"]`;
+  const works = await client.fetch(query);
+  console.log(works.length);
+  return {
+    props:{
+      works
+    }
+  }
 
-export const urlFor = (source) => builder.image(source);
+}
