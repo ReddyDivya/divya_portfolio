9cbb5576fb5dd39d358cdbe514c60ae7324c5b9b
Divya <divyareddy0205@gmail.com>
2023-04-07 21:04:31 -0700

    works

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -1,22 +1,16 @@
 //working with images using sanity
+
+// Import using ESM URL imports in environments that supports it
 import {createClient} from '@sanity/client';
 import ImageUrlBuilder  from '@sanity/image-url';
 
-// export const client = createClient({
-//   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-//   dataset: 'production',
-//   apiVersion: '2023-04-06',
-//   useCdn: true,
-//   token: process.env.REACT_APP_SANITY_TOKEN,
-// });
-
-
 export const client = createClient({
-  projectId: "8rkkr4xz",
-  dataset: "production",
-//   apiVersion: '2023-04-06',
-  useCdn: false,
-//   token: 'skAbD8Pik1s2V4o98TvC9um8SOJuQY65QCMmaQryJpUd8Y9eTLbWoUYOtWBwQPLtPlTcBZudtb2gRi2Fz04NCF6pXQULdbWGJmYiiD0QCO718DcNM3oos0a84CpEpyXYUPtUYbsLUhYbUbfkOaj1mRxvshLt9BK38JyLMmtzBlBMRoNML2pt',
+  projectId: '8rkkr4xz',
+  dataset: 'production',
+  apiVersion: '2023-04-07', // use current date (YYYY-MM-DD) to target the latest API version
+  useCdn: true,
+  token: process.env.REACT_APP_SANITY_TOKEN,//Only if you want to update content with the client
+
 });
 
 const builder = ImageUrlBuilder(client);
