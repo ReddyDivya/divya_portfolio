6de0cc23ed0bb7a2bd94045d171cbf3759f7c7c3
Divya <divyareddy0205@gmail.com>
2023-04-14 10:43:01 -0700

    testimonial

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import './Testimonial.scss';
+import {HiChevronLeft, HiChevronRight} from "react-icons/hi";
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
+
 
 const Testimonial = () => {
   return (
