4692aeeea2ed0d9a365c0f9234c45e24310b62f3
Divya <divyareddy0205@gmail.com>
2023-04-14 10:55:34 -0700

    testimonials

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Testimonial.scss';
 import {HiChevronLeft, HiChevronRight} from "react-icons/hi";
 import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 
-
 const Testimonial = () => {
+  const [brands, setBrands] = useState([]);
+  const [testimonials, setTestimonials] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   return (
     <div>Testimonial</div>
   )
