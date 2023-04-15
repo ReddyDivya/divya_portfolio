fe8d53290c7d3449e80be569d41065af75404b30
Divya <divyareddy0205@gmail.com>
2023-04-15 13:00:08 -0700

    updating portfolio

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -33,6 +33,7 @@ const Testimonial = () => {
     <>
       {testimonials.length && (
         <>
+          <h2 className="head-text">People I've worked with have said some nice things...</h2>
           <div className="app__testimonial-item app__flex">
             <img src={urlFor(testimonials[currentIndex].imgurl)} alt={testimonials[currentIndex].name} />
             <div className="app__testimonial-content">
