6e1e8ac9fd1e0dca319c4adcef7e1aee02398889
Divya <divyareddy0205@gmail.com>
2023-04-17 21:38:18 -0700

    blog component

--- frontend_react/src/container/Blogs/Blogs.jsx
+++ frontend_react/src/container/Blogs/Blogs.jsx
@@ -6,7 +6,6 @@ import {SiHashnode} from "react-icons/si";
 
 const Blogs = () => {
     const [posts, setPosts] = useState([]);
-    const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
 
     const fetchPosts = async (query) => {
       const data = await fetch('https://api.hashnode.com', {
@@ -46,47 +45,22 @@ const Blogs = () => {
   return (
     <>
         <h2 className="head-text">Blogs</h2>
-        {/* <motion.div
+        
+        <motion.div
           transition={{ duration: 0.5, delayChildren: 0.5 }}
           className="app__blog-portfolio"
         >
-            {posts.map((post, index) => (
-
-              <div className="app__blog-item app__flex" key={index}>
-                <div
-                  className="app__blog-img app__flex"
-                >
-                    <a key={index} 
-                        href={`https://blog.reddydivya.dev/${post.slug}`} >
-                        <div>
-                            <img src={post.coverImage} alt={post.title}/>
-                        </div>
-                        <div className="app__blog-content app__flex">
-                          <h4 className="bold-text">{post.title}</h4>
-                          <p className="p-text" style={{ marginTop: 10 }}>{post.brief}</p>
-                        </div>
-                    </a>
-                </div>
-              </div>  
-            ))}
-        </motion.div> */}
-
-        <motion.div
-          animate={animateCard}
-          transition={{ duration: 0.5, delayChildren: 0.5 }}
-          className="app__work-portfolio"
-        >
         {posts.map((post, index) => (
-          <div className="app__work-item app__flex" key={index}>
+          <div className="app__blog-item app__flex" key={index}>
             <div
-              className="app__work-img app__flex"
+              className="app__blog-img app__flex"
             >
               <img src={post.coverImage} alt={post.title} />
 
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
                 transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
-                className="app__work-hover app__flex"
+                className="app__blog-hover app__flex"
               >
                 <a href={`https://thedivyareddyy.hashnode.dev/${post.slug}`} target="_blank" rel="noreferrer">
                   <motion.div
@@ -101,7 +75,7 @@ const Blogs = () => {
               </motion.div>
             </div>
 
-            <div className="app__work-content app__flex">
+            <div className="app__blog-content app__flex">
               <h4 className="bold-text">{post.title}</h4>
               <p className="p-text" style={{ marginTop: 10 }}>{post.brief}</p>
             </div>
