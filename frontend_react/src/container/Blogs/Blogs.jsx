857890d969b128723bbafb967faac0bd198416eb
Divya <divyareddy0205@gmail.com>
2023-04-17 21:13:01 -0700

    blog component

--- frontend_react/src/container/Blogs/Blogs.jsx
+++ frontend_react/src/container/Blogs/Blogs.jsx
@@ -1,15 +1,33 @@
 import React, {useState, useEffect} from 'react';
 import  "./Blogs.scss";
 import {AppWrap, MotionWrap} from "../../wrapper";
+import { motion } from 'framer-motion';
+import {SiHashnode} from "react-icons/si";
 
 const Blogs = () => {
-    const [blogs, setBlogs] = useState({posts:''});
+    const [posts, setPosts] = useState([]);
+    const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
 
-    //to fetch blogs from hashnode api [https://api.hashnode.com]
-    const query = `query {
-        user(username:"ReddyDivya")
-        {
-          publication{
+    const fetchPosts = async (query) => {
+      const data = await fetch('https://api.hashnode.com', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({ query }),
+      })
+
+      const response = await data.json();
+      setPosts(response.data.user.publication.posts);
+    };
+
+    useEffect(()=>{
+      
+      //to fetch blogs from hashnode api [https://api.hashnode.com]
+      const query = `
+      {
+        user(username: "ReddyDivya") {
+          publication {
             posts{
               slug
               title
@@ -18,42 +36,79 @@ const Blogs = () => {
             }
           }
         }
-      }`;
+      }
+      `;
 
-    const fetchBlogPosts = async () => {
-        const response = await fetch('https://api.hashnode.com', {
-            method: 'POST',
-            headers: {
-              'Content-type': 'application/json',
-            },
-            body: JSON.stringify({ query }),
-          });
-
-          const blogsData = await response.json(); //.json() method parses the JSON response into a JS object literal
-          setBlogs({ posts: blogsData.data.user.publication.posts});
-          
-          console.log(blogsData.data.user.publication.posts);   
-          console.log(blogs.posts)
-    }
-
-    useEffect(()=>{
-        fetchBlogPosts();
+      fetchPosts(query); 
     }, [])
+
 
   return (
     <>
-        <div className="head-text">Blogs</div>
-        <div>
-            { 
-                // blogs.posts.maps((blog, index) => (
-                //     <p>{blog.posts[0].title}</p>
-                //     // <a key={index} href={`https://blog.ReddyDivya.dev/${blog.slug}`}>
-                //     //     <img src={blog.coverImage} alt={blog.title}/>
-                //     //     <p>{blog.brief}</p>
-                //     // </a>
-                // ))
-            }
-        </div>
+        <h2 className="head-text">Blogs</h2>
+        {/* <motion.div
+          transition={{ duration: 0.5, delayChildren: 0.5 }}
+          className="app__blog-portfolio"
+        >
+            {posts.map((post, index) => (
+
+              <div className="app__blog-item app__flex" key={index}>
+                <div
+                  className="app__blog-img app__flex"
+                >
+                    <a key={index} 
+                        href={`https://blog.reddydivya.dev/${post.slug}`} >
+                        <div>
+                            <img src={post.coverImage} alt={post.title}/>
+                        </div>
+                        <div className="app__blog-content app__flex">
+                          <h4 className="bold-text">{post.title}</h4>
+                          <p className="p-text" style={{ marginTop: 10 }}>{post.brief}</p>
+                        </div>
+                    </a>
+                </div>
+              </div>  
+            ))}
+        </motion.div> */}
+
+        <motion.div
+          animate={animateCard}
+          transition={{ duration: 0.5, delayChildren: 0.5 }}
+          className="app__work-portfolio"
+        >
+        {posts.map((post, index) => (
+          <div className="app__work-item app__flex" key={index}>
+            <div
+              className="app__work-img app__flex"
+            >
+              <img src={post.coverImage} alt={post.title} />
+
+              <motion.div
+                whileHover={{ opacity: [0, 1] }}
+                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                className="app__work-hover app__flex"
+              >
+                <a href={`https://blog.reddydivya.dev/${post.slug}`} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.90] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <SiHashnode />
+                  </motion.div>
+                </a>
+              </motion.div>
+            </div>
+
+            <div className="app__work-content app__flex">
+              <h4 className="bold-text">{post.title}</h4>
+              <p className="p-text" style={{ marginTop: 10 }}>{post.brief}</p>
+            </div>
+          </div>
+        ))}
+      </motion.div>
+        
     </>
   )
 }
