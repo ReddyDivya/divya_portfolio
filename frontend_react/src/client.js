28875b67062a32c09b90e1cef915064d5bcd60a7
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2023-07-23 04:18:09 -0700

    Update client.js

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -12,12 +12,12 @@ export const client = createClient({
   
   //options
   //process.env => environment variables
-  projectId: "8rkkr4xz",
-  dataset: 'portfolio',
-  apiVersion: '2023-04-01',
+  projectId: "",
+  dataset: '',
+  apiVersion: '',
   useCdn: true,
   //token is important for creating a field in sanity[see contact]
-  token:'skAbD8Pik1s2V4o98TvC9um8SOJuQY65QCMmaQryJpUd8Y9eTLbWoUYOtWBwQPLtPlTcBZudtb2gRi2Fz04NCF6pXQULdbWGJmYiiD0QCO718DcNM3oos0a84CpEpyXYUPtUYbsLUhYbUbfkOaj1mRxvshLt9BK38JyLMmtzBlBMRoNML2pt'
+  token:''
 });
 
 //creates an image URL builder instance by invoking the imageUrlBuilder function and passing in the client instance created in the previous step.
