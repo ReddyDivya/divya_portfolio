8ccca260d5bff740fbfc46cf910e463c432f01d2
Divya <divyareddy0205@gmail.com>
2024-06-10 21:22:12 -0700

    re-working on portfolio

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -20,33 +20,24 @@ const Skills = () => {
     <>
       <h2 className="head-text">Technical Skills</h2>
       <div className="app__skills-container">
+        <motion.div className="app__skills-list">
           {skills.map((skill) => (
-              <motion.div className="app__skills-list">
-                <div
-                  className="app__flex"
-                  style={{ backgroundColor: "#F0EEEE" }}
-                >
-                  <img src={urlFor(skill.icon)} alt={skill.name} />
-                </div>
-              </motion.div>
+            <motion.div
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__skills-item app__flex"
+              key={skill.name}
+            >
+              <div
+                className="app__flex"
+                style={{ backgroundColor: skill.bgColor }}
+              >
+                <img src={urlFor(skill.icon)} alt={skill.name} />
+              </div>
+              <p className="p-text">{skill.name}</p>
+            </motion.div>
           ))}
-
-          {/* <div
-              className="app__flex"
-              style={{ backgroundColor: "#F0EEEE" }}
-          >
-            
-          </div> */}
-          {/* <div className="app__flex">
-            <div>
-              <img src="https://skillicons.dev/icons?i=html,css,js,react,redux,tailwind,bootstrap,git,github" alt="skills"/>
-            </div>
-            <br/>
-            <div>
-              <img src="https://skillicons.dev/icons?i=materialui,nodejs,postman,styledcomponents,express,firebase,visualstudio,vite,netlify" alt="skills"/>
-            </div>
-          </div> */}
-        {/* </motion.div> */}
+        </motion.div>
       </div>
     </>
   );
